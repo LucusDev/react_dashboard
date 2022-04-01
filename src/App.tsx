361e285f7417a5749af2860988b6ad7397e155af
Htetaungshine11 <newbtcearner@gmail.com>
2022-04-02 01:19:40 +0630

    sidebar ui finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/home/Home";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 import New from "./pages/new/New";
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,17 @@
 import "./home.scss";
-
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 const Home: React.FC = () => {
-  return <div className="home">home</div>;
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        home container
+      </div>
+    </div>
+  );
 };
 
 export default Home;
