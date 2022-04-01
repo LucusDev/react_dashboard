139b2aab40df7ab5b9d4aab54481300baa598189
Htetaungshine11 <33309639+Htetaungshine11@users.noreply.github.com>
2022-04-02 00:19:31 +0630

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Home from "./pages/home/Home";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+          <Route index element={<Home />}></Route>
+          <Route path="login" element={<Login />} />
+
+          <Route path="users">
+            <Route index element={<List></List>}></Route>
+            <Route path=":userId" element={<Single />}></Route>
+            <Route path="new" element={<New></New>}></Route>
+          </Route>
+          <Route path="products">
+            <Route index element={<List></List>}></Route>
+            <Route path=":productId" element={<Single />}></Route>
+            <Route path="new" element={<New></New>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,7 @@
+import "./home.scss";
+
+const Home: React.FC = () => {
+  return <div className="home">home</div>;
+};
+
+export default Home;
